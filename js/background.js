f9910e4be12e032fec05d2383cb594cce043207c
jay <devejayloper@gmail.com>
2021-11-03 15:47:19 +0900

    ✨ background images

---
+++ js/background.js
@@ -0,0 +1,23 @@
+const images = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+// img라는 element(html 요소)를 만들어 반환한다.
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+// appendChild는 body에 html을 맨뒤에 추가한다.
