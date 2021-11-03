415ca4382c1da619d646f4f68c90e34d91501f8e
jay <devejayloper@gmail.com>
2021-11-03 12:04:32 +0900

    ✨ clock

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,15 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
+getClock();
+// 1초를 기다려서 setInterval이 실행되지 않고 바로 실행하게끔 한다.
+
+setInterval(getClock, 1000);
