68962885074e8b171415ec7e0dd040f3ba256208
jay <devejayloper@gmail.com>
2021-11-05 12:47:46 +0900

    ✨ weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "459e0928992d940affb9913a874af043";
+
+function geoOk(position) {
+  const lat = position.coords.latitude; // 위도
+  const lon = position.coords.longitude; // 경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:nth-child(2)");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function geoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(geoOk, geoError);
